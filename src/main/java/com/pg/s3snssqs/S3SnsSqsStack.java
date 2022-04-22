92078d2ee57986ee3b3a50b1c429bae343beb6ee
kunip004 <prajesh.x.kunimal.-nd@disney.com>
2022-04-22 13:33:55 -0400

    Branch/S3SNSSQS : added comments .

--- src/main/java/com/pg/s3snssqs/S3SnsSqsStack.java
+++ src/main/java/com/pg/s3snssqs/S3SnsSqsStack.java
@@ -1,25 +1,14 @@
 package com.pg.s3snssqs;
 
-import com.myorg.HitCounter;
-import com.myorg.HitCounterProps;
-import org.jetbrains.annotations.NotNull;
 import software.amazon.awscdk.core.*;
-import software.amazon.awscdk.services.apigateway.LambdaRestApi;
-import software.amazon.awscdk.services.lambda.Code;
-import software.amazon.awscdk.services.lambda.Function;
-import software.amazon.awscdk.services.lambda.Runtime;
-import software.amazon.awscdk.services.s3.*;
+import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.s3.EventType;
+import software.amazon.awscdk.services.s3.NotificationKeyFilter;
 import software.amazon.awscdk.services.s3.notifications.SnsDestination;
-import software.amazon.awscdk.services.sns.*;
-import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
-import software.amazon.awscdk.services.sqs.DeadLetterQueue;
-import software.amazon.awscdk.services.sqs.IQueue;
-import software.amazon.awscdk.services.sqs.Queue;
-/*
-import software.amazon.awscdk.core.Duration;
+import software.amazon.awscdk.services.sns.ITopic;
 import software.amazon.awscdk.services.sns.Topic;
 import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
-import software.amazon.awscdk.services.sqs.Queue;*/
+import software.amazon.awscdk.services.sqs.Queue;
 
 public class S3SnsSqsStack extends Stack {
     public S3SnsSqsStack(final Construct parent, final String id) {
@@ -30,30 +19,30 @@ public class S3SnsSqsStack extends Stack {
         super(parent, id, props);
 
         //create bucket.
-        Bucket bucket =  Bucket.Builder.create(this,"graph-pk-bkt-test")
+        Bucket bucket = Bucket.Builder.create(this, "graph-pk-bkt-test")
                 .bucketName("graph-loader-pk-bkt-test")
                 .build();
 
         //create topic.
-        ITopic topic = Topic.Builder.create(this,"graph-pk-topic-test")
+        ITopic topic = Topic.Builder.create(this, "graph-pk-topic-test")
                 .topicName("graph-loader-pk-topic-test")
-                 .build();
+                .build();
 
         //create queue.
-        Queue queue = Queue.Builder.create(this,"graph-loader-pk-queue-test")
+        Queue queue = Queue.Builder.create(this, "graph-loader-pk-queue-test")
                 .queueName("graph-loader-pk-queue-test")
                 .build();
 
         //add filter to the bucket.
-        NotificationKeyFilter.Builder notificationFilter =  NotificationKeyFilter.builder();
+        NotificationKeyFilter.Builder notificationFilter = NotificationKeyFilter.builder();
         NotificationKeyFilter notificationKeyFilter = notificationFilter.suffix("*.parquet").build();
-        bucket.addEventNotification(EventType.OBJECT_CREATED, new SnsDestination(topic) ,notificationKeyFilter);
+        bucket.addEventNotification(EventType.OBJECT_CREATED, new SnsDestination(topic), notificationKeyFilter);
 
         //Subscribe sqs to topic
-       topic.addSubscription(new SqsSubscription(queue));
+        topic.addSubscription(new SqsSubscription(queue));
 
-       //Cout the stack topic arn.
+        //Cout the stack topic arn.
         CfnOutputProps cfnOutputProps = CfnOutputProps.builder().value(topic.getTopicArn()).description("topic arn valuess").build();
-        CfnOutput CfnOutput =   new CfnOutput(this,"snsTopicArn",cfnOutputProps);
+        CfnOutput CfnOutput = new CfnOutput(this, "snsTopicArn", cfnOutputProps);
     }
 }
